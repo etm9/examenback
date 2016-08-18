d0b8d90048517cd12ccbb2271018490ce162b23b
Edgar <etm9@jelpmi.mx>
2016-08-17 22:00:20 -0500

    ultimo

--- test/models/alumno_test.rb
+++ test/models/alumno_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AlumnoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  require 'test_helper'
+
+  class SalonTest < ActiveSupport::TestCase
+
+   def setup
+    @alumno = student(:one)
+    end
+   test 'ENG Delating in cascade   ESP Borrado en Cascada alumnos' do
+    @alumno.destroy
+    assert_empty Ranking.where(student:@student.id)
+    end
 end
